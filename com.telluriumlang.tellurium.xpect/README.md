1419796ee691b604d959573bb90cbaf944b03f26
Steve Ou <me@oushaobin.cn>
2021-04-08 18:20:12 +0100

    [Improve] Add Xpect Support

---
+++ com.telluriumlang.tellurium.xpect/README.md
@@ -0,0 +1,7 @@
+# Xpect Test for Tellurium
+
+This is an Xpect Junit Test for Tellurium Project. 
+
+Please note, this project is not managed by the Maven. You may need to set up your Xpect environment before running the test. 
+
+[Xpect's Website](http://www.xpect-tests.org/)
---
+++ com.telluriumlang.tellurium.xpect/src/com/telluriumlang/tellurium/xpect/TelluriumXPectGeneratorTest.java
@@ -0,0 +1,22 @@
+package com.telluriumlang.tellurium.xpect;
+
+import org.eclipse.xpect.runner.XpectRunner;
+import org.eclipse.xpect.runner.XpectSuiteClasses;
+import org.eclipse.xpect.xtext.lib.tests.GeneratorTest;
+import org.eclipse.xpect.xtext.lib.tests.LinkingTest;
+import org.eclipse.xpect.xtext.lib.tests.ResourceDescriptionTest;
+import org.eclipse.xpect.xtext.lib.tests.ScopingTest;
+import org.eclipse.xpect.xtext.lib.tests.ValidationTest;
+import org.eclipse.xpect.xtext.xbase.lib.tests.JvmModelInferrerTest;
+import org.junit.runner.RunWith;
+
+@XpectSuiteClasses({ JvmModelInferrerTest.class, //
+	LinkingTest.class, //
+	ResourceDescriptionTest.class, //
+	ScopingTest.class, //
+	ValidationTest.class,  //
+})
+@RunWith(XpectRunner.class)
+public class TelluriumXPectGeneratorTest extends GeneratorTest {
+
+}
